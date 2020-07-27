87f2815711b670a75055e80d046d583e7e992c4a
Inku0 <46846071+Inku0@users.noreply.github.com>
2020-07-28 01:22:14 +0300

    Bug fix and copying functionality

--- passman.py
+++ passman.py
@@ -1,9 +1,19 @@
 import random
 import sqlite3
+import pyperclip
 
 conn = sqlite3.connect('passwords.db')
 
 c = conn.cursor()
+
+
+def close_connection():
+    # Save (commit) the changes
+    conn.commit()
+
+    # Close the connection
+    conn.close()
+
 
 wish = input('What do you wish to do? (N)ew password, (E)dit a password or (C)opy a password: ')
 
@@ -12,8 +22,7 @@ if wish == 'N':
     letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
     length = input('Length: ')
-    
-    # Check if input is an integer
+
     try:
         val = int(length)
     except ValueError:
@@ -22,11 +31,10 @@ if wish == 'N':
 
     given_name = input('Name: ')
 
-    generated_password = list(letters)[random.randint(0, 94)]
-    
-    # Add random letters until the correct length has been achieved
-    for i in range(int(length)-1):
-        generated_password += list(letters)[random.randint(0, 94)]
+    generated_password = list(letters)[random.randint(0, 93)]
+
+    for i in range(int(length)):
+        generated_password += list(letters)[random.randint(0, 93)]
 
     print(generated_password)
 
@@ -38,19 +46,22 @@ if wish == 'N':
     c.execute("INSERT INTO passwords (name, password) VALUES(?, ?)",
               (given_name, generated_password))
 
-    # Save (commit) the changes
-    conn.commit()
-
-    # Close the connection
-    conn.close()
+    close_connection()
 
 elif wish == 'E':
     pass
 elif wish == 'C':
-    # Select all the names from the SQLite database
     c.execute("SELECT name FROM main.passwords")
-    
-    # Print all of the selected names
+
     print(c.fetchall())
 
     which_one = input('Which one?: ')
+
+    c.execute("SELECT password FROM main.passwords WHERE name=?", (which_one,))
+
+    password_to_be_copied = str(c.fetchone())
+    
+    # Slice the string so that the parentheses don't get copied
+    pyperclip.copy(password_to_be_copied[1:-2])
+
+    close_connection()
